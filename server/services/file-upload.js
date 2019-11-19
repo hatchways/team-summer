c5523ad3de7946bf0f9b792b1bfa45a79b56723c
DevDelvo <kevinbdelvo@gmail.com>
2019-11-19 17:21:11 -0500

    added filefilter error handler

--- server/services/file-upload.js
+++ server/services/file-upload.js
@@ -1,5 +1,4 @@
 const aws = require('aws-sdk');
-const express = require('express');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
@@ -13,10 +12,20 @@ aws.config.update({
 
 const s3 = new aws.S3();
 
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid MIME Type, only JPEG or PNG'), false)
+    }
+}
+
 const upload = multer({
+    fileFilter,
     storage: multerS3({
-        s3: s3,
+        s3,
         bucket: AWS_BUCKET,
+        acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, { fieldName: 'TESTING_META_DATA!' });
         },
