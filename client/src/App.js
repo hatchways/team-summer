a3c1bb1eb6acaca2e38ce4f527f614a113eb689e
Evan Kemp <ekp.web@gmail.com>
2019-11-18 18:11:41 -0800

    Add navbar without navigation; remove App.css; Theme adjustment

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react'
+import {MuiThemeProvider, AppBar, Toolbar, Typography, makeStyles} from '@material-ui/core'
+import {BrowserRouter, Route} from 'react-router-dom'
 
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import {theme} from './themes/theme'
+import LandingPage from './pages/Landing'
 
-import "./App.css";
+import SvgProductLaunchLogo from './components/ProductLaunchLogo'
 
-function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+const App = () => {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <AppBar position="static" color="#ffffff">
+                <Toolbar>
+                    <SvgProductLaunchLogo style={{marginRight: 22}}/>
+                    <Typography variant="h1">Product Launch</Typography>
+                </Toolbar>
+            </AppBar>
+            <BrowserRouter>
+                <Route path="/" component={LandingPage}/>
+            </BrowserRouter>
+        </MuiThemeProvider>
+    )
 }
 
-export default App;
+export default App
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -2,9 +2,13 @@ import { createMuiTheme } from "@material-ui/core";
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: '"Roboto"'
+    fontFamily: '"Roboto"',
+    h1: {
+      fontSize: '1.5em',
+      fontWeight: 'semi-bold'
+    }
   },
-  primary: "#f04040",
+  primary: "#69E781",
   secondary: "#1f1f1f",
   error: "#d8000c",
   bgcolor: "#f6f6f6"
