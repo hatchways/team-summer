3987822c45c1c1ae7d4380d39b445e51411c613c
tsyaeger <tsyaeger@gmail.com>
2019-11-27 19:50:26 -0800

    remove user project population from jwt verification isAuth

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,45 +1,33 @@
 const { decodeToken } = require('../utils');
-const {User} = require('../models');
+const { User } = require('../models');
 
-exports.isAuth = (req, res, next) => {
+exports.isAuth = async (req, res, next) => {
   const { authorization } = req.headers;
   if (authorization) {
     const token = authorization.split(' ')[1];
-    const { payload } = decodeToken(token);
+    const { payload } = await decodeToken(token);
     if (payload && payload._id) {
-      User.findById(payload._id)
-        .populate('projects')
-        .exec((err, user) => {
-          if (err || !user) {
-            res.status(400).json({
-              error: 'User not found'
-            });
-          } else {
-            const { _id, name, email, projects } = user;
-            req.profile = { _id, name, email, projects };
-            next();
-          }
-        });
-    } else {
-      return res.status(403).send({ error: 'Access denied' });
+      req.user = payload;
+      next();
     }
   } else {
     return res.status(403).send({ error: 'Access denied' });
   }
 };
 
-//IS THIS NEEDED BY FUTURE ROUTES?
-// exports.userById = (req, res, next, id) => {
-//   User.findById(id)
-//     .populate('projects')
-//     .exec((err, user) => {
-//       if (err || !user) {
-//         return res.status(400).json({
-//           error: 'User not found'
-//         });
-//       }
-//       const { _id, name, email, projects } = user;
-//       req.profile = { _id, name, email, projects };
-//       next();
-//     });
-// };
+exports.userById = (req, res, next, id) => {
+  User.findById(id)
+    .populate('projects')
+    .exec((err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          error: 'User not found'
+        });
+      }
+      const { _id, name, email, projects } = user;
+      req.profile = { _id, name, email, projects };
+      next();
+    });
+};
+
+
