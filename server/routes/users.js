999b34a19930443f79d639e72c63ab53b62fdb59
tsyaeger <tsyaeger@gmail.com>
2019-11-30 12:21:04 -0800

    remove comments

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,18 +11,3 @@ router.get('/:id', (req, res) => {
 router.param('id', userById);
 
 module.exports = router;
-
-
-
-  
-// const express = require('express');
-// const router = express.Router();
-// const getUser = require('../controllers/authController.api').userById;
-
-// router.get('/users/:id', (req, res) => {
-//   res.status(200).json(req.profile);
-// });
-
-// router.param('id', getUser);
-
-// module.exports = router;
