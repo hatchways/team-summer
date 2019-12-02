20c2a6037f66ec2fd18d969a9a9b798f0ce80a9c
Evan Kemp <ekp.web@gmail.com>
2019-12-02 10:03:04 -0800

    Responsiveness adjustments

--- client/src/styles/ExploreSyles.js
+++ client/src/styles/ExploreSyles.js
@@ -6,17 +6,36 @@ export const styles = (theme) => ({
   }
 });
 
-export const Main = styled('main')({
-  padding: '50px 80px',
+export const Main = styled('main')(({theme}) => ({
+  padding: '50px 30px',
   display: 'flex',
   flexDirection: 'column',
-  justifyContent: 'center'
-});
+  justifyContent: 'center',
+
+  [theme.breakpoints.up('md')]: {
+    padding: '50px 80px'
+  }
+}));
 
 export const FilterContainer = styled('div')(({ theme, ...props }) => ({
+  margin: '0 auto 30px auto',
   display: 'flex',
-  justifyContent: 'space-evenly',
+  flexDirection: 'column',
+  alignItems: 'center',
 
-  width: '30%',
-  margin: '0 auto 70px auto',
+  '& .MuiInputBase-root': {
+    marginBottom: 20,
+    width: '100%',
+
+    [theme.breakpoints.up('md')]: {
+      width: 'unset'
+    },
+  },
+
+  [theme.breakpoints.up('md')]: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '50%',
+    margin: '0 auto 70px auto',
+  }
 }));
