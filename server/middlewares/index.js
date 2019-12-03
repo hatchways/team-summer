c84cc76a4b68aa5c1f3bacd3e6643e094015853b
DevDelvo <kevinbdelvo@gmail.com>
2019-12-03 03:30:25 -0500

    reverted changes to user controller and middlewares

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -8,6 +8,7 @@ exports.isAuth = async (req, res, next) => {
     const token = authorization.split(' ')[1];
     const { payload } = await decodeToken(token);
     if (payload && payload._id && payload._id === userId) {
+      console.log(userId, payload._id)
       req.user = payload;
       next();
     }
@@ -26,8 +27,7 @@ exports.userById = (req, res, next, id) => {
         });
       }
       const { _id, name, email, about, location, projects, profilePic } = user;
-      // req.profile = { _id, name, about, email, projects, location, profilePic };
-      req.profile = { _id, name, email, projects };
+      req.profile = { _id, name, about, email, projects, location, profilePic };
       next();
     });
 };
