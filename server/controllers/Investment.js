15bdfb8a718dadfc85c055e5adfd544c2a984f2a
tsyaeger <tsyaeger@gmail.com>
2019-12-02 17:50:42 -0800

    refactor format dollar fn

--- server/controllers/Investment.js
+++ server/controllers/Investment.js
@@ -3,15 +3,16 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const {User, Project, Investment} = require('../models');
 
 
-
 //I've adjusted this to be called prior to the payment but needs a refactor
 //might be cleaner with promise chains
 const invest = async (userId, projectId, token, investmentAmount) => {
+  const dollarAmount = toDollarsWithCents(investmentAmount)
+
   try {
     const investment = await Investment.create({
       user: ObjectId(userId),
       project: ObjectId(projectId),
-      value: fromDollarsToCents(investmentAmount)
+      value: dollarAmount
     });
     await User.updateOne({ _id: userId }, { $push: { investments: investment._id } });
     await Project.updateOne(
@@ -19,7 +20,7 @@ const invest = async (userId, projectId, token, investmentAmount) => {
       {
         $inc: {
           'funding.donorCount': 1,
-          'funding.fundingTotal': fromDollarsToCents(investmentAmount)
+          'funding.fundingTotal': dollarAmount
         }
       }
     );
@@ -29,14 +30,15 @@ const invest = async (userId, projectId, token, investmentAmount) => {
   }
 };
 
-const fromDollarsToCents = (amount) => parseInt(amount * 100);
+const toDollarsWithCents = (amount) => parseInt(amount * 100);
 
 exports.makePayment = async (req, res) => {
   const { userId, projectId, token, investmentAmount } = req.body;
+  const dollarAmount = toDollarsWithCents(investmentAmount)
   const t = token.token
 
   const order = {
-      amount: fromDollarsToCents(investmentAmount),
+      amount: dollarAmount,
       currency: 'USD',
       source: t.id, 
       description: 'investment'
