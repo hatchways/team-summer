aa72ef088c127cb96d520977e7c130111dd9028c
Evan Kemp <ekp.web@gmail.com>
2019-12-05 01:15:10 -0800

    Small comment adjustments

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -16,14 +16,15 @@ module.exports = (io) => {
 
     // Socket event listeners
     /*
+    * https://socket.io/docs/emit-cheatsheet/
     * You can send a response inside a listener by one of the following:
     *
-    * Socket: Emits the message in the room EXCEPT for the sender
+    * Socket: Emits value to all the clients in the room (by default all clients) EXCEPT for the sender
     *   (to a different room then default use .to(room).emit())
-    * * socket.emit('event', () => {...})
+    * * socket.emit('event', value)
     *
     * io: Emits the message to all the clients connected (or .to(room)) (including person who triggered it)
-    * * io.emit('event', () => {...})
+    * * io.emit('event', value)
     * */
 
     /* Connects userId to a room, allows for the ability to send to a user id */
