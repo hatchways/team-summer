8cde450c78e8fa68c5cfd25c10c5565fddcfdb20
tsyaeger <tsyaeger@gmail.com>
2019-12-05 22:06:39 -0800

    remove comment

--- client/src/api/payments.js
+++ client/src/api/payments.js
@@ -1,7 +1,6 @@
 import httpClient from './httpClient';
 
 export const pay = (projectId, token, investmentAmount) => {
-  console.log("amount", investmentAmount )
     return httpClient.post(`/investments/invest`, {
       projectId,
       token,
