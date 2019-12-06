8afd4e84304d2f7a4f6262f21000b449cd23deae
tsyaeger <tsyaeger@gmail.com>
2019-12-05 22:09:55 -0800

    remove comment

--- client/src/components/AlertBadge.js
+++ client/src/components/AlertBadge.js
@@ -4,9 +4,7 @@ import Badge from '@material-ui/core/Badge';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { makeStyles } from '@material-ui/core/styles'
 
-
 const useStyles = makeStyles(theme => ({
-    
     margin: {
         margin: theme.spacing(2),
     },
@@ -14,7 +12,6 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(0, 2),
     },
 }));
-
 
 export default function AlertBadge({ alerts }) {
     const classes = useStyles();
