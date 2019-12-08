19562f0768759e37d0685139e85ffef954a7532d
tsyaeger <tsyaeger@gmail.com>
2019-12-08 02:28:09 -0800

    BE create a charge and investment with stripe

--- server/controllers/Investment.js
+++ server/controllers/Investment.js
@@ -1,40 +1,12 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { ObjectId } = require('mongoose').Types;
 const { User, Project, Investment } = require('../models');
 
-exports.addInvestment = async (req, res) => {
-  const { value, projectId } = req.body;
-  const user = req.user
-
-  try {
-    const investment = await Investment.create({
-      user: ObjectId(user._id),
-      project: ObjectId(projectId),
-      value: parseInt(value)
-    });
-    await User.updateOne({ _id: user._id }, { $push: { investments: investment._id } });
-    await Project.updateOne(
-      { _id: projectId },
-      {
-        $inc: {
-          'funding.donorCount': 1,
-          'funding.fundingTotal': parseInt(value)
-        }
-      }
-    );
-    res.status(201).send(investment);
-  } catch (err) {
-    res.status(400).json({
-      error: "User Investment's could not be updated.",
-      err
-    });
-  }
-};
 
 exports.getInvestment = async (req, res) => {
   const { id } = req.params;
 
   Investment.findById(id)
-    // .populate('user')
     .populate('projects')
     .exec((err, investment) => {
       if (err) {
@@ -45,4 +17,66 @@ exports.getInvestment = async (req, res) => {
       }
       return res.status(200).json(investment);
     })
+}
+
+
+const invest = async (userId, projectId, investmentAmount) => {
+  const dollarAmount = toDollarsWithCents(investmentAmount)
+
+  try {
+    const investment = await Investment.create({
+      user: ObjectId(userId),
+      project: ObjectId(projectId),
+      value: dollarAmount
+    });
+    await User.updateOne(
+      { _id: userId }, 
+      { $push: { investments: investment._id } }
+    );
+    await Project.updateOne(
+      { _id: projectId },
+      { $inc: {
+          'funding.donorCount': 1,
+          'funding.fundingTotal': dollarAmount
+        }
+      }
+    );
+    return investment;
+  } catch (err) {
+    res.status(400).json({
+      error: "User Investment's could not be updated."
+    });
+  }
+};
+
+//adds cents to dollar amount
+//stripe counts the last two digits on an int as cents
+const toDollarsWithCents = (amount) => parseInt(amount * 100);
+
+exports.makePayment = async (req, res) => {
+  const { projectId, token, investmentAmount } = req.body;
+  const userId = req.user._id
+  const dollarAmount = toDollarsWithCents(investmentAmount)
+  const stripeToken = token.token
+
+  const order = {
+    amount: dollarAmount,
+    currency: 'USD',
+    source: stripeToken.id,
+    description: 'investment'
+  }
+  try {
+    const investment = await invest(userId, projectId, investmentAmount);
+    if (investment) {
+      stripe.charges.create(order, (err, charge) => {
+        if (err) {
+          return res.status(400).json({ message: 'an error occurred' });
+        } else {
+          return res.status(200).json({ investment });
+        }
+      })
+    }
+  } catch (err) {
+    return res.status(400).json({ message: 'an error occurred' });
+  }
 }
