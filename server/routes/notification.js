83b05298174f15d814adc0e9ee5932013fc159b3
DevDelvo <kevinbdelvo@gmail.com>
2019-12-09 03:04:59 -0500

    refactored events to be notifications

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { isAuth } = require('../middlewares');
-const events = require('../controllers/Event');
+const notification = require('../controllers/Notification');
 
-router.get('/', isAuth, events.getNotifications);
-router.post('/', isAuth, events.createNotification);
+router.get('/', isAuth, notification.getNotifications);
+router.post('/', isAuth, notification.createNotification);
 
 module.exports = router;
