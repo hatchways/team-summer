e8ea19c1b337820d18c83c197c962fd310a8e1e8
Evan Kemp <ekp.web@gmail.com>
2019-12-11 10:35:06 -0800

    Formatting

--- server/test/ping.spec.js
+++ server/test/ping.spec.js
@@ -1,22 +1,22 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 require('dotenv').config();
-const app = require("../app.js");
+const app = require('../app.js');
 
 chai.should();
 chai.use(chaiHttp);
 
-describe("/POST ping", () => {
-  it("it should return 400", done => {
+describe('Connection test', () => {
+  it('Request should return 400', (done) => {
     chai
       .request(app)
       .post(`/ping/`)
-      .send({ teamName: "Shums" })
+      .send({ teamName: 'Shums' })
       .end((err, res) => {
         res.should.have.status(400);
         res.body.should.have
-          .property("response")
-          .eql("Shums is not part of the team. Modify your .env");
+          .property('response')
+          .eql('Shums is not part of the team. Modify your .env');
         done();
       });
   });
