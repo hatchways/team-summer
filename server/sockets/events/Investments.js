2d729e3c40a2b4e952b21f8ec33d4287a9184e0e
DevDelvo <kevinbdelvo@gmail.com>
2019-12-12 19:28:02 -0500

    removed console log in investment socket

--- server/sockets/events/Investments.js
+++ server/sockets/events/Investments.js
@@ -1,5 +1,4 @@
 exports.receiveInvestment = (io, socket) => ({ id, name, projectName }) => {
     socket.join(id);
-    console.log('name', name, projectName)
     socket.to(id).emit('newInvestment', { name, projectName });
 };
