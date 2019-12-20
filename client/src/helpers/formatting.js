a01566078cc9a628b66264d5e35b0d11286fcb8c
tsyaeger <tsyaeger@gmail.com>
2019-12-19 23:24:40 -0800

    fixes daysLeft - from daysFrom date to days until

--- client/src/helpers/formatting.js
+++ client/src/helpers/formatting.js
@@ -1,3 +1,11 @@
+import moment from 'moment'
+
 export const toDollars = (num) => {
     return `$${num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
 } 
+
+export const daysLeft = (fundingDeadline) => {
+    const deadlineDate = moment(fundingDeadline);
+    const todaysDate = moment();
+    return deadlineDate.diff(todaysDate, 'days');
+}
